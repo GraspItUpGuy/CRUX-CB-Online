21a23fa7a3d08c4d9e47c7985cc694cab048885c
Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>
2019-07-06 23:34:48 +0530

    pattern 17
    Signed-off-by: Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>

---
+++ Patterns/PatternsAndDesigns/src/pattern17.java
@@ -0,0 +1,41 @@
+public class pattern17 {
+    public static void main(String[] args) {
+        int n = 7; // rows
+        int nst = n / 2; // stars
+        int nsp = 1;    // spaces
+
+        int row = 1;
+        while (row <= n) {
+
+            // stars
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print(" * ");
+            }
+            // spaces
+            for (int csp = 1; csp <= nsp; csp++) {
+                System.out.print("   ");
+            }
+            // stars
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print(" * ");
+            }
+
+            // prepration
+            System.out.println();
+            if(row <= n/2){
+                nsp = nsp+2;
+                nst = nst-1;
+            }else {
+                nsp = nsp-2;
+                nst = nst +1g;
+
+            }
+
+            row ++;
+
+
+        }
+    }
+
+}
+
