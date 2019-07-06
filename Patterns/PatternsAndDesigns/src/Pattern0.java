d7301072ced5e2268700176967b7bd661a9335f4
Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>
2019-07-06 13:39:15 +0530

    pattrn 1
    Signed-off-by: Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>

---
+++ Patterns/PatternsAndDesigns/src/Pattern0.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.TimeUnit;
+
+
+public class Pattern0 {
+    public static void main(String[] args){
+        int n=5;
+        for(int i=0;i<n;i++){
+            System.out.println("*");
+
+        }
+    }
+}
--- Patterns/PatternsAndDesigns/src/Pattern1.java
+++ Patterns/PatternsAndDesigns/src/Pattern1.java
@@ -1,8 +1,11 @@
 public class Pattern1 {
     public static void main(String[] args){
         int n=5;
-        for(int i=0;i<n;i++){
-            System.out.println("*");
+        for(int i=0;i<n; i++){
+            for(int j=0; j<n;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
         }
     }
 }
