a72a07db5c74172a0f931e90d98e58904c40c795
Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>
2019-07-06 21:37:13 +0530

    pattern 10
    Signed-off-by: Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>

--- Patterns/PatternsAndDesigns/src/pattern9.java
+++ Patterns/PatternsAndDesigns/src/pattern9.java
@@ -3,11 +3,8 @@ public class pattern9 {
         int n=5;
         for(int i=0; i<n; i++){
             for(int j=0; j< (2*n) -1 ; j++){
-                if((i+j >= n-1)&&(j-i<g n)){
-                    System.out.print(" * ");
-                }
-                else{
-                    System.out.print("   ");
+                if((i+j>=n-1)&&(i+j)<=2*n-2){
+                    System.out.print("");
                 }
             }
             System.out.println();
