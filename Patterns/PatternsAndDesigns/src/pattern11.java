227186672c02300249f4c24c8cd505b201053945
Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>
2019-07-07 17:00:26 +0530

    pattern 11
    Signed-off-by: Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>

--- Patterns/PatternsAndDesigns/src/pattern11.java
+++ Patterns/PatternsAndDesigns/src/pattern11.java
@@ -1,29 +1,32 @@
 public class pattern11 {
-    public static void main(String[] args){
-        int n=5;
 
-        int nsp = n-1;
-        int nst =1;
+        public static void main(String[] args){
+            int n=5;
 
-        int row=1;
-        while(row<=n){
-            //spaces
-            for(int csp =1; csp<=nsp; csp++){
-                System.out.print(" ");
-            }
-            // stars
-            for(int cst = 1; cst<=nst; cst++){
-                if(cst %2g ==0){
-                    System.out.print("!");
-                }else{
-                    System.out.print("*");
+            int nsp = n-1;
+            int nst =1;
+
+            int row=1;
+            while(row<=n){
+                //spaces
+                for(int csp =1; csp<=nsp; csp++){
+                    System.out.print("   ");
                 }
-            }
+                // stars
+                for(int cst = 1; cst<=nst; cst++){
+                    if(cst %2 ==0){
+                        System.out.print("   ");
+                    }else{
+                        System.out.print(" * ");
+                    }
+                }
 
-            System.out.println();
-            nsp--;
-            nst+=2;
-            row++;
+                System.out.println();
+                nsp--;
+                nst+=2;
+                row++;
+            }
         }
     }
-}
+
+
---
+++ Patterns/PatternsAndDesigns/src/pattern12.java
@@ -0,0 +1,29 @@
+public class pattern12 {
+    public static void main(String[] args){
+        int n=5;
+
+        int nsp = n-1;
+        int nst =1;
+
+        int row=1;
+        while(row<=n){
+            //spaces
+            for(int csp =1; csp<=nsp; csp++){
+                System.out.print(" ");
+            }
+            // stars
+            for(int cst = 1; cst<=nst; cst++){
+                if(cst %2 ==0){
+                    System.out.print("!");
+                }else{
+                    System.out.print("*");
+                }
+            }
+
+            System.out.println();
+            nsp--;
+            nst+=2;
+            row++;
+        }
+    }
+}
