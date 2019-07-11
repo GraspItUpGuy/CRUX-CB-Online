f9e61d6a25ed1e0ef7a8b3854ff7dff81e030fe0
Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>
2019-07-11 11:47:38 +0530

    bubble sort
    Signed-off-by: Puneet Kumar <puneetkumar@PuneetKumars-MacBook-Air.local>

--- FunctionsAndArrays/src/bubbleSort.java
+++ FunctionsAndArrays/src/bubbleSort.java
@@ -1,8 +1,44 @@
 public class bubbleSort {
     public static void main(String args[]){
-       int[] arr = { 31, 17, 47, 76, 6, 29};
+       int[] array = { 31, 17, 47, 76, 6, 29,0, 19, 85};
+        System.out.print("Unsorted array\n");
+        for(int i=0; i<array.length;i++){
+            System.out.print(array[i] + ",");
+        }
 
-       int i=0;
+       bubbleSort(array);
+
+        System.out.print("\nSorted array is \n");
+        for(int i=0; i<array.length;i++){
+            System.out.print(array[i] + ",");
+        }
+
+
+
        
     }
+    public static void bubbleSort(int[] arr){
+        int temp;
+
+        System.out.println("\nSee the steps");
+        for(int counter =0; counter<arr.length -1; counter ++){
+           for(int j=0; j<arr.length-1; j++){
+
+               if(arr[j+1]< arr[j]){
+                  temp = arr[j+1];
+                  arr[j+1] = arr[j];
+                  arr[j]  =temp;
+               }
+
+           }
+
+           
+            for(int i=0; i<arr.length;i++){
+                System.out.print(arr[i] + ",");
+            }
+        }
+    }
+
+
+
 }
