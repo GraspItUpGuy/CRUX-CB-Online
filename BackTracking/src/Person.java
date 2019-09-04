817d6a103356454898d67b8bd3bb92bd73a8df93
GraspItUpGuy <birmaan.puneet19@gmail.com>
2019-09-04 23:18:31 +0530

    access modifiers
    Signed-off-by: GraspItUpGuy <birmaan.puneet19@gmail.com>

--- BackTracking/src/Person.java
+++ BackTracking/src/Person.java
@@ -1,5 +1,24 @@
 public class Person {
-    String name;
-    int age;
+     private String name;
+    private int age;
 
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name ;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
+
+// private       => visible inside same class
+// public        => visible everywhere
+// protected     => will study later
+// default       => visible inside same package
--- BackTracking/src/PersonClient.java
+++ BackTracking/src/PersonClient.java
@@ -3,8 +3,10 @@ public class PersonClient {
         Person p1 = new Person();
         Person p2 = new Person();
         Person p3 = new Person();
-        System.out.println(p1.age + "\n" + p1.name);
-        System.out.println(p2.age + "\n" + p2.name);
-        System.out.println(p3.age + "\n" + p3.name);
+//        System.out.println(p1.age + "\n" + p1.name);
+//        System.out.println(p2.age + "\n" + p2.name);
+    //    System.out.println(p3.age + "\n" + p3.name);
+        System.out.println((p1.getName()));
+        System.out.println((p1.getAge()));
     }
 }
