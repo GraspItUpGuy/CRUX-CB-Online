4201cc6a2fa08c42909f912ca9ce96340b20211e
GraspItUpGuy <birmaan.puneet19@gmail.com>
2019-09-04 22:29:08 +0530

    classes
    Signed-off-by: GraspItUpGuy <birmaan.puneet19@gmail.com>

---
+++ BackTracking/src/PersonClient.java
@@ -0,0 +1,10 @@
+public class PersonClient {
+    public static void main(String[] args){
+        Person p1 = new Person();
+        Person p2 = new Person();
+        Person p3 = new Person();
+        System.out.println(p1.age + "\n" + p1.name);
+        System.out.println(p2.age + "\n" + p2.name);
+        System.out.println(p3.age + "\n" + p3.name);
+    }
+}
