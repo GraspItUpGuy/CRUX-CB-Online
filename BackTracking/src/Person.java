64f5dd9b7b6245c32aefffe95574d6e5a21ea3f4
GraspItUpGuy <birmaan.puneet19@gmail.com>
2019-09-04 23:27:31 +0530

    constructor
    Signed-off-by: GraspItUpGuy <birmaan.puneet19@gmail.com>

--- BackTracking/src/Person.java
+++ BackTracking/src/Person.java
@@ -2,20 +2,44 @@ public class Person {
      private String name;
     private int age;
 
-    public String getName(){
-        return this.name;
+    // constructor
+    // initialise the instance of particular object
+    // Person p = new Person();
+    // have no return type, not even void
+    // have same name as the class
+    // look like a function
+    Person(){
+        System.out.println("I am the default constructor");
     }
-    public void setName(String name){
-        this.name = name ;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
+    public Person(int age){
         this.age = age;
+        System.out.println(age);
     }
+    public Person(String name){
+        this.name = name;
+        System.out.println(name);
+    }
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+        System.out.println(name + age);
+    }
+
+
+//    public String getName(){
+//        return this.name;
+//    }
+//    public void setName(String name){
+//        this.name = name ;
+//    }
+//
+//    public int getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(int age) {
+//        this.age = age;
+//    }
 }
 
 // private       => visible inside same class
--- BackTracking/src/PersonClient.java
+++ BackTracking/src/PersonClient.java
@@ -1,12 +1,13 @@
 public class PersonClient {
     public static void main(String[] args){
         Person p1 = new Person();
-        Person p2 = new Person();
-        Person p3 = new Person();
-//        System.out.println(p1.age + "\n" + p1.name);
-//        System.out.println(p2.age + "\n" + p2.name);
-    //    System.out.println(p3.age + "\n" + p3.name);
-        System.out.println((p1.getName()));
-        System.out.println((p1.getAge()));
+        Person p2 = new Person("Pankaj");
+        Person p3 = new Person(10);
+        Person p4 = new Person("Puneet ",10);
+////        System.out.println(p1.age + "\n" + p1.name);
+////        System.out.println(p2.age + "\n" + p2.name);
+//    //    System.out.println(p3.age + "\n" + p3.name);
+//        System.out.println((p1.getName()));
+//        System.out.println((p1.getAge()));
     }
 }
