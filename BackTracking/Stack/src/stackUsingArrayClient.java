3311cc9972387d289614b9ed51a6000bbf4fa935
GraspItUpGuy <birmaan.puneet19@gmail.com>
2019-09-06 14:19:49 +0530

    stack using array
    Signed-off-by: GraspItUpGuy <birmaan.puneet19@gmail.com>

---
+++ BackTracking/Stack/src/stackUsingArrayClient.java
@@ -0,0 +1,33 @@
+public class stackUsingArrayClient {
+
+    public static void main(String[] args) throws Exception {
+
+//        stackUsingArray s = new stackUsingArray(8);
+//        s.push(10);
+//        s.push(20);
+//        s.push(30);
+//        s.push(40);
+//        s.push(50);
+//        s.push(60);
+//        s.display();
+//
+//        System.out.println(s.pop());
+//        System.out.println(s.pop());
+//
+//        s.push(100);
+//        s.push(200);
+//        s.push(6300);
+//        while(s.isEmpty() == true){
+//            s.pop();
+//            s.display();
+//        }
+//
+//
+//        s.display();
+//
+//    }
+
+
+    }
+
+}
