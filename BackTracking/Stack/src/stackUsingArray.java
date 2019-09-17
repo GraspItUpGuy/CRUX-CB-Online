f8855c7ed1b39a3e387bc5d5f226e56fcc090e5b
GraspItUpGuy <birmaan.puneet19@gmail.com>
2019-09-17 15:06:43 +0530

    stack using array
    Signed-off-by: GraspItUpGuy <birmaan.puneet19@gmail.com>

--- BackTracking/Stack/src/stackUsingArray.java
+++ BackTracking/Stack/src/stackUsingArray.java
@@ -1,125 +1,63 @@
-public class stackUsingArray {
-//    protected int[] data;
-//    protected int tos;
-//
-//    public stackUsingArray() {
-//        data = new int[5];
-//        tos = -1;
-//    }
-//
-//    public stackUsingArray(int cap) {
-//        data = new int[cap];
-//        tos = -1;
-//    }
-//
-//    public void push(int item) throws Exception {
-//
-//        if (isFull()) {
-//            throw new Exception("Stack is Full.");
-//        }
-//
-//        tos++;
-//        data[tos] = item;
-//    }
-//
-//    public int pop() throws Exception {
-//
-//        if (isEmpty()) {
-//            throw new Exception("Stack is Empty.");
-//        }
-//
-//        int temp = data[tos];
-//        data[tos] = 0;
-//        tos--;
-//        return temp;
-//    }
-//
-//    public int peek() throws Exception {
-//
-//        if (isEmpty()) {
-//            throw new Exception("Stack is Empty.");
-//        }
-//
-//        int temp = data[tos];
-//        return temp;
-//    }
-//
-//    public int size() {
-//        return tos + 1;
-//    }
-//
-//    public boolean isEmpty() {
-//        return size() == 0;
-//    }
-//
-//    public boolean isFull() {
-//        return size() == data.length;
-//    }
-//
-//    public void display() {
-//
-//        System.out.println("-------------------");
-//
-//        for (int i = tos; i >= 0; i--) {
-//            System.out.print(data[i] + " ");
-//        }
-//        System.out.println("\n-------------------");
-//    }
-
-    private int data[] ;
+public class stackUsingArray {    //  LIFO
+    private int[] data;
     private int top;
+       int size;
 
-    public  stackUsingArray(){
-        this.data = new int[5];
+    public stackUsingArray() {
+        this.size = 10;
+    }
+
+    public stackUsingArray(int capacity) throws Exception{
+        if(capacity < 1){
+            throw new Exception("invalid Capacity");
+        }
+        this.data = new int[capacity];
+        this.size = capacity;
         this.top = -1;
     }
-    public  stackUsingArray(int capacity)throws Exception{
-        if(capacity < 1){
-            throw new Exception("capacity can't be less than 1 ");
-        }else {
-            this.data = new int[capacity];
-            this.top = -1;
-        }
+
+    public int getSize(){
+        return this.top + 1 ;
     }
-    public int size(){
-        return this.top +1 ;
-    }
+
     public boolean isEmpty(){
-        return (this.size() == 0);
-    }
-    public void push(int value) throws Exception{
-        if(this.size() == this.data.length){
-            throw new Exception("Stack is full");
+        if(this.getSize() == 0){
+            return true;
+        }else{
+            return false;
         }
-        this.top ++;
-        this.data[this.top] = value;
-
     }
 
-    public int pop()throws Exception{
-        if(this.size()==0){
-            throw new Exception("stack is empty");
+public void push(int val)throws Exception{
+        if(this.getSize()== this.data.length){
+            throw new Exception("Stack Overflow");
+        }
+        this.top++;
+        this.data[this.top] = val;
+}
+
+public int pop()throws Exception{
+        if(this.getSize() == 0){
+            throw new Exception("Stack Empty");
         }
         int rv = this.data[this.top];
         this.data[this.top] = 0;
         this.top --;
         return rv;
-    }
-
-    public int top()throws Exception{
-        if(this.size()==0){
-            throw new Exception("stack is empty");
+}
+ public int top()throws Exception{
+        if(this.getSize()==0){
+            throw new Exception("Stack is empty");
         }
-        int rv = this.data[this.top];
+     int rv = this.data[this.top];
         return rv;
-    }
-
-    public void display(){
-        for(int i = this.top; i>=0; i--){
-            System.out.print(this.data[i] + ",");
-        }
-        System.out.println("end");
-    }
+ }
+ public void display(){
+     for (int i = this.top; i >=0 ; i--) {
+         System.out.print(this.data[i] + "=>");
+     }
+     System.out.println("END");
+ }
 
 
 }
--- BackTracking/Stack/src/stackUsingArrayClient.java
+++ BackTracking/Stack/src/stackUsingArrayClient.java
@@ -1,52 +1,21 @@
 public class stackUsingArrayClient {
+    public static void main(String[] args) throws Exception{
+        stackUsingArray stack = new stackUsingArray(5);
 
-    public static void main(String[] args) throws Exception {
-
-//        stackUsingArray s = new stackUsingArray(8);
-//        s.push(10);
-//        s.push(20);
-//        s.push(30);
-//        s.push(40);
-//        s.push(50);
-//        s.push(60);
-//        s.display();
-//
-//        System.out.println(s.pop());
-//        System.out.println(s.pop());
-//
-//        s.push(100);
-//        s.push(200);
-//        s.push(6300);
-//        while(s.isEmpty() == true){
-//            s.pop();
-//            s.display();
-//        }
-//
-//
-//        s.display();
-//
-//    }
-
-        stackUsingArray stack = new stackUsingArray();
+//        stack.push(10);;
+//        stack.display();
         for (int i = 1; i <=5 ; i++) {
             stack.push(i*10);
             stack.display();
+            System.out.println(stack.top());
         }
-        System.out.println();
-
-//        System.out.println( stack.top());
-//        System.out.println(stack.pop());
-//        stack.display();
-//        System.out.println(stack.top());
-
-
-        while (!stack.isEmpty()){
-            stack.display();
-            stack.pop();
-        }
-
+       // stack.push(60);
+      while(!stack.isEmpty()) { stack.pop();
         stack.display();
-
+        System.out.println(stack.top());}
     }
-
 }
+
+
+// complexity is O(1)
+///  used in browsers when we view previors or forward visited pages
