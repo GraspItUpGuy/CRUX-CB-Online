ee1e0c0a986a50eef5a0f848f5c2a3bb02e44358
GraspItUpGuy <birmaan.puneet19@gmail.com>
2019-09-18 22:50:42 +0530

    inheritence + stack and que
    Signed-off-by: GraspItUpGuy <birmaan.puneet19@gmail.com>

---
+++ BackTracking/Stack/src/inheritence_client.java
@@ -0,0 +1,9 @@
+public class inheritence_client {
+    public static void main(String[] args){
+        inheritence obj = new inheritence();
+        System.out.println(obj.d);
+        System.out.println(obj.d1);
+        child c = new child();
+
+    }
+}
--- BackTracking/queue/src/queUsingArrayClient.java
+++ BackTracking/queue/src/queUsingArrayClient.java
@@ -12,5 +12,15 @@ public class queUsingArrayClient {
         System.out.println(que.deQue());
         que.display();
         System.out.println(que.getFront());
+
+        while(!que.isEmpty()){
+            que.display();;
+            System.out.println("front = " + que.getFront());
+            que.deQue();
+        }
+        que.enQue(10);
+        System.out.println(que.getFront());
+        que.display();
+
     }
 }
